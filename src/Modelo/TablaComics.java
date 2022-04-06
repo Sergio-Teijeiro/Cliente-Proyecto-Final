85ef62469644b48bd203c780ab4ae8281f42728d
sergio <sergio@PORTATIL-SERGIO>
2022-04-06 20:44:28 +0200

    Corrección de un constructor y mejora de rendimiento.
    Se van añadiendo las colecciones a una lista si no existen. Si existe la
    colección del cómic en la lista, ya no se realiza una petición al
    servidor sino que la colección se recupera de la lista en memoria, lo
    que se tradujo en mucha más velocidad de consulta. Antes la barra de
    desplazamiento de la tabla iba muy lenta.

--- src/Modelo/TablaComics.java
+++ src/Modelo/TablaComics.java
@@ -12,13 +12,15 @@ public class TablaComics extends AbstractTableModel {
 	private ArrayList<Numero> listaComics;
 	private String[] columnas = {"Título","Colección"};
 	private Socket socketCliente;
+	private ArrayList<Coleccion> colecciones = new ArrayList<>();
 	
 	public static Coleccion coleccion;
 	
-	public TablaComics(ArrayList<Numero> listaComics,String[] columnas) {
+	public TablaComics(ArrayList<Numero> listaComics,String[] columnas, Socket skCliente) {
 		super();
 		this.listaComics = listaComics;
 		this.columnas = columnas;
+		this.socketCliente = skCliente;
 	}
 	
 	public TablaComics(ArrayList<Numero> listaComics, Socket skCliente) {
@@ -42,22 +44,39 @@ public class TablaComics extends AbstractTableModel {
 		if (row != -1 && row < listaComics.size()) {
 			Numero n = listaComics.get(row);
 			coleccion = null;
+			boolean yaAgregada = false;
+			Coleccion aux = null;
 			
-			HiloCliente hilo = new HiloCliente(socketCliente,"colByComic",n,coleccion);
-			hilo.start();
-			
-			try {
-				hilo.join();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			for (Coleccion c : colecciones) {
+				if (c.getId() == n.getIdColeccion()) {
+					yaAgregada = true;
+					aux = c;
+					break;
+				}
 			}
 			
+			if (!yaAgregada) {
+				HiloCliente hilo = new HiloCliente(socketCliente,"colByComic",n,coleccion);
+				hilo.start();
+				
+				try {
+					hilo.join();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				colecciones.add(coleccion);
+			}	
 			
 			switch (col) {
 			case 0:
 				return n.getTitulo();
-			case 1: return coleccion.getNombre();
+			case 1: if (!yaAgregada) {
+						return coleccion.getNombre();
+					} else {
+						return aux.getNombre();
+					}
 			default:
 				return null;
 			}
