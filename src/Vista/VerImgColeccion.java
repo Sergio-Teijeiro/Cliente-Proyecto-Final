9e5fe8cd2ed57c470f9941c3c677aaffadbf6107
sergio <sergio@PORTATIL-SERGIO>
2022-04-27 20:26:44 +0200

    Cargar imagen de la colección en pantalla

--- src/Vista/VerImgColeccion.java
+++ src/Vista/VerImgColeccion.java
@@ -1,14 +1,23 @@
 package Vista;
 
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.swing.JDialog;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
 
 public class VerImgColeccion extends JDialog {
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -22,12 +31,38 @@ public class VerImgColeccion extends JDialog {
 			}
 		});
 	}
-
-	/**
-	 * Create the dialog.
-	 */
+	
 	public VerImgColeccion(byte[] img) {
-		setBounds(100, 100, 450, 300);
+		setResizable(false);
+		
+		setBounds(0, 0, 443, 680);
+		setMinimumSize(new Dimension(443, 680));
+		setMaximumSize(new Dimension(443, 680));
+		setLocationRelativeTo(null);
+		setIconImage(
+				Toolkit.getDefaultToolkit().getImage(PantallaPrincipal.class.getResource("/img/app_icon.png")));
+		setModalityType(ModalityType.APPLICATION_MODAL);
+		
+		JButton btnImgColeccion = new JButton();
+		btnImgColeccion.setBounds(new Rectangle(0, 0, 443, 680));
+		
+		BufferedImage bufImg = null;
+		try {
+			bufImg = ImageIO.read(new ByteArrayInputStream(img));
+		} catch (IOException ex) {
+			Logger.getLogger(DetalleComic.class.getName()).log(Level.SEVERE, null, ex);
+		}
+
+		ImageIcon iconoEscala = new ImageIcon(bufImg.getScaledInstance(btnImgColeccion.getWidth(),
+				btnImgColeccion.getHeight(), java.awt.Image.SCALE_FAST));
+		
+		btnImgColeccion.setFocusPainted(false);
+		btnImgColeccion.setContentAreaFilled(false);
+		btnImgColeccion.setBorderPainted(false);
+
+		btnImgColeccion.setIcon(iconoEscala);
+		
+		getContentPane().add(btnImgColeccion, BorderLayout.CENTER);
 
 	}
 
