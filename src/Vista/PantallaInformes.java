ac2b11dfc500f8ab97ed50417e870b349bbea96f
sergio <sergio@PORTATIL-SERGIO>
2022-04-28 19:23:20 +0200

    Estructura general

--- src/Vista/PantallaInformes.java
+++ src/Vista/PantallaInformes.java
@@ -1,22 +1,43 @@
 package Vista;
 
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+
+import Controlador.HiloCliente;
 
 public class PantallaInformes {
 
-	private JFrame frame;
+	JFrame frmInformes;
+	private String[] opciones = { "Sí", "No" };
+	private String mensajeSalir = "¿Deseas salir de la aplicación?", cerrarPrograma = "Cerrar programa";
+	private String tituloPantalla = "Creación de informes";
+	private JMenu menuComics, menuColecciones, menuInformes;
+	private JMenuItem itemBusqueda,itemComics,itemColecciones,itemInformes;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					PantallaInformes window = new PantallaInformes();
-					window.frame.setVisible(true);
+					PantallaInformes window = new PantallaInformes(null);
+					window.frmInformes.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -24,20 +45,118 @@ public class PantallaInformes {
 		});
 	}
 
-	/**
-	 * Create the application.
-	 */
-	public PantallaInformes() {
-		initialize();
+	public PantallaInformes(Socket skCliente) {
+		initialize(skCliente);
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	private void initialize(Socket skCliente) {
+		frmInformes = new JFrame();
+		frmInformes.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				// dialogo confirmacion
+				ImageIcon icono = new ImageIcon(PantallaPrincipal.class.getResource("/img/app_icon.png"));
+				ImageIcon iconoEscala = new ImageIcon(
+						icono.getImage().getScaledInstance(50, 50, java.awt.Image.SCALE_FAST));
+				JLabel lblPregunta = new JLabel(mensajeSalir);
+				lblPregunta.setFont(new Font("Caladea", Font.PLAIN, 16));
+				int respuesta = JOptionPane.showOptionDialog(frmInformes, lblPregunta,
+						cerrarPrograma, JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, iconoEscala,
+						opciones, opciones[1]);
+
+				if (respuesta == 1) { // si elige no cerrar, especifico que no haga nada
+					frmInformes.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+
+				} else if (respuesta == 0) {
+					frmInformes.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+					
+					//si cierra el programa, se cierra la conexion del cliente y se envia la orden de salir
+					try {
+			            if (skCliente != null) {
+			                //mando orden salir y cierro conexion
+			                HiloCliente hilo = new HiloCliente(skCliente, "fin", null);
+			                hilo.start();
+			                hilo.join();
+
+			                skCliente.close();
+			            }
+			        } catch (IOException ex) {
+			            Logger.getLogger(PantallaBusqueda.class.getName()).log(Level.SEVERE, null, ex);
+			        } catch (InterruptedException ex) {
+			            Logger.getLogger(PantallaBusqueda.class.getName()).log(Level.SEVERE, null, ex);
+			        }
+					
+				} else { // si cierra el dialogo de confirmacion, no cierra la ventana principal
+					frmInformes.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+				}
+			}
+		});
+		
+		frmInformes.setMinimumSize(new Dimension(890, 760));
+		frmInformes.setTitle(tituloPantalla);
+		frmInformes.setBounds(0, 0, 450, 300);
+		frmInformes.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frmInformes.setLocationRelativeTo(null);
+		frmInformes.setIconImage(
+				Toolkit.getDefaultToolkit().getImage(PantallaPrincipal.class.getResource("/img/app_icon.png")));
+		
+		JMenuBar menuBar = new JMenuBar();
+		frmInformes.setJMenuBar(menuBar);
+		
+		menuComics = new JMenu("Cómics");
+		menuComics.setFont(new Font("Caladea", Font.PLAIN, 16));
+		menuBar.add(menuComics);
+		
+		itemBusqueda = new JMenuItem("Consultar cómics");
+		itemBusqueda.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frmInformes.dispose();
+
+				PantallaBusqueda p = new PantallaBusqueda(skCliente);
+				p.frmBusqueda.setVisible(true);
+			}
+		});
+		itemBusqueda.setFont(new Font("Caladea", Font.PLAIN, 16));
+		menuComics.add(itemBusqueda);
+		
+		itemComics = new JMenuItem("Gestión cómics");
+		itemComics.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frmInformes.dispose();
+
+				OperacionesComics o = new OperacionesComics(skCliente);
+				o.frmComics.setVisible(true);
+			}
+		});		
+		itemComics.setFont(new Font("Caladea", Font.PLAIN, 16));
+		menuComics.add(itemComics);
+		
+		menuColecciones = new JMenu("Colecciones");
+		menuColecciones.setFont(new Font("Caladea", Font.PLAIN, 16));
+		menuBar.add(menuColecciones);
+		
+		itemColecciones = new JMenuItem("Gestión colecciones");
+		itemColecciones.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frmInformes.dispose();
+
+				PantallaColecciones p = new PantallaColecciones(skCliente);
+				p.frmColecciones.setVisible(true);
+			}
+		});
+		itemColecciones.setFont(new Font("Caladea", Font.PLAIN, 16));
+		menuColecciones.add(itemColecciones);
+		
+		menuInformes = new JMenu("Informes");
+		menuInformes.setFont(new Font("Caladea", Font.PLAIN, 16));
+		menuBar.add(menuInformes);
+		
+		itemInformes = new JMenuItem("Crear informes");
+		itemInformes.setFont(new Font("Caladea", Font.PLAIN, 16));
+		menuInformes.add(itemInformes);
 	}
 
 }
