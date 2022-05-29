c9d90e4f0924375e2fc07670d29625c905cb9ff7
sergio <sergio@PORTATIL-SERGIO>
2022-05-29 16:56:09 +0200

    Documentación añadida

--- src/Fuentes/Fuentes.java
+++ src/Fuentes/Fuentes.java
@@ -3,12 +3,24 @@ package Fuentes;
 import java.awt.Font;
 import java.io.InputStream;
 
+/**
+ * Clase para gestionar fuentes personalizadas
+ * @author sergio
+ *
+ */
 public class Fuentes {
 	private Font font = null;
     public static String BOUNCY = "Bouncy.otf";
     
     // Font.PLAIN = 0 , Font.BOLD = 1 , Font.ITALIC = 2
     
+    /**
+     * Obtiene una fuente determinada
+     * @param fontName Nombre de la fuente
+     * @param estilo Estilo de la fuente: 0 (Font.PLAIN), 1 (Font.BOLD) o 2 (Font.ITALIC)
+     * @param tamanho Tamaño de la fuente
+     * @return Fuente creada a partir de un fichero .otf con el nombre fontName
+     */
     public Font getFuente(String fontName, int estilo, float tamanho)
     {
          try {
